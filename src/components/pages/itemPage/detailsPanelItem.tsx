5930eb2040b4bf1562a3209e38846e82745803aa
raishoemi <raishoemi@gmail.com>
2022-04-22 16:07:05 +0300

    Make organism name in protein details a link

--- src/components/pages/itemPage/detailsPanelItem.tsx
+++ src/components/pages/itemPage/detailsPanelItem.tsx
@@ -7,12 +7,16 @@ type Props = {
     href?: string,
     extraNameProps?: {},
     extraValueProps?: {}
+    isLink?: boolean
 }
 
 const DetailsPanelItem: React.FC<Props> = (props) => (
     <Typography.Paragraph>
         <Typography.Text {...props.extraNameProps} disabled={!props.value} strong>{props.name}: </Typography.Text>
-        <Typography.Text {...props.extraValueProps} disabled={!props.value} italic={!props.value}>{props.value || 'No data available'}</Typography.Text>
+        {props.isLink ?
+            <Typography.Link {...props.extraValueProps} disabled={!props.value} italic={!props.value}>{props.value || 'No data available'}</Typography.Link> :
+            <Typography.Text {...props.extraValueProps} disabled={!props.value} italic={!props.value}>{props.value || 'No data available'}</Typography.Text>
+        }
     </Typography.Paragraph>
 );
 
--- src/components/pages/proteinPage/index.tsx
+++ src/components/pages/proteinPage/index.tsx
@@ -1,6 +1,6 @@
 import { Typography } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getProtein } from '../../../api/protein';
 import { Protein } from '../../../types';
 import { ItemPage, LoadingItemPage } from '../itemPage';
@@ -9,6 +9,7 @@ import DetailsPanelItem from '../itemPage/detailsPanelItem';
 
 const ProteinPage: React.FC<{}> = () => {
     const { id } = useParams();
+    const navigate = useNavigate();
     const [protein, setProtein] = useState<Protein>();
 
     useEffect(() => {
@@ -29,7 +30,7 @@ const ProteinPage: React.FC<{}> = () => {
                     <DetailsPanelItem extraValueProps={{ copyable: true }} name='Uniprot ID' value={protein.id} />
                     <DetailsPanelItem name='Name' value={protein.name} />
                     <DetailsPanelItem name='Evidence' value={protein.evidence} />
-                    <DetailsPanelItem name='Organism' value={protein.taxonomy.name} extraValueProps={{ href: `/taxonomy/${protein.taxonomy.id}` }} />
+                    <DetailsPanelItem name='Organism' isLink value={protein.taxonomy.name} extraValueProps={{ onClick: () => navigate(`/taxonomy/${protein.taxonomy.id}`) }} />
                     <DetailsPanelItem name='Gene' value={protein.geneName} />
                     <DetailsPanelItem name='Chromosome' value={protein.proteome.chromosome} />
                 </>,
